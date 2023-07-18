cbb774169612af78d9030cd379c25cec67661435
Yuliia Hubarenko <xlapushka@ukr.net>
2023-07-18 20:30:21 +0300

    ready

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -42,6 +42,22 @@ export class App extends Component {
     }));
   }
 
+  componentDidMount() {
+    const contacts = localStorage.getItem ('contacts');
+    const contactsFromLocalStorageParsed = JSON.parse(contacts);
+    if (contactsFromLocalStorageParsed) {
+      this.setState ({
+        contacts: contactsFromLocalStorageParsed
+      })
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+    }
+  }
+
   
   render() {
     const normilizedFilter = this.state.filter.toLowerCase();
